272380615bc350eac29a247839a6b378f7696139
Meromen <meromen1375@gmail.com>
2019-09-29 02:38:59 +1000

    Add build config

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
+  productionSourceMap: false,
+  filenameHashing: false,
+  outputDir: 'build',
+  lintOnSave: process.env.NODE_ENV !== 'production'
+}
